82196854cd5d1f2d67469d981d87d0ee739f9608
Tprezioso <tommyprezioso@gmail.com>
2020-04-13 19:03:38 -0400

    Initial Commit

---
+++ SignInWIthAppleDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SignInWIthAppleDemo
+//
+//  Created by Thomas Prezioso on 4/13/20.
+//  Copyright © 2020 Thomas Prezioso. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
